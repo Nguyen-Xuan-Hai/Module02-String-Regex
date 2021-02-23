7db7bff02ed25719fdcc7d663e4f8bea767f00ce
Nguyen-Xuan-Hai <kute3bino@gmail.com>
2021-02-23 08:19:21 +0700

    bai-tap

---
+++ Validate-Class-Name.php
@@ -0,0 +1,17 @@
+<?php
+function valiedataClass($arr){
+
+    $regexp = '/^[CAP]{1}+[0-9]{4}+[GHIKML]{1}$/';
+
+    if (preg_match_all($regexp,$arr)){
+        echo "Dung";
+    } else {
+        echo "Sai";
+    }
+}
+
+echo valiedataClass("C0318G");
+echo "<br>";
+echo valiedataClass("M0318G");
+echo "<br>";
+echo valiedataClass("P0323A");
